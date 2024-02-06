2f496a84bfa6d3b61cd7712ba028a4ccef68774e
aydakikio <127264152+aydakikio@users.noreply.github.com>
2024-02-06 15:53:12 +0330

    Version 0.1
    added web block and web unlock

--- blocker.sh
+++ blocker.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/bash
 
 status="$1"
+timer="$2"
 blocklist=()
 websites=("www.zoomg.ir" "www.ninisite.com" "www.zoomit.ir" )
 ips=()
 current_ip=$(ifconfig | grep 'inet ' | awk '{print $2}')
-		
-	
-	for ip in "$ips" ;do 
+
+
+	for ip in "$ips" ;do
 	    if [ "$current_ip" != "$ip" ] ; then
 		ips+=($(ifconfig | grep 'inet ' | awk '{print $2 }'))
-		
-	    fi
-	done 	
 
-	
+	    fi
+	done
+
+
 	  for ip in "${ips[@]}" ; do
 	      for website in "${websites[@]}";do
 
                    blocklist+=("${ip}  ${website}")
 	      done
 	  done
-	
+
           for item in "${blocklist[@]}" ; do
-		echo "$item"
+		case "$1" in
+		"web_block")
+                echo "$item" | sudo tee -a /etc/hosts >/dev/null
+		;; 
+		"web_unlock")
+                sudo sed -i "/$item/d" /etc/hosts
+		;;
+		esac
 	  done
 
-         
+
 
 
 
